b3b31a2bf32c3ccf1a8771108e1b24ed934469f7
Aaron Sikes <aaron@sikes.io>
2017-02-05 13:50:13 -0500

    Returning models in route handlers seems to work

--- tests/dummy/mirage/config.js
+++ tests/dummy/mirage/config.js
@@ -1,14 +1,11 @@
 export default function() {
   this.get('/patients/:id', function({patients}, {params}) {
-    let p = patients.find(params.id);
-    return this.serialize(p);
+    return patients.find(params.id);
   });
   this.get('/documents/:id', function({documents}, {params}) {
-    let doc = documents.find(params.id);
-    return this.serialize(doc);
+    return documents.find(params.id);
   });
   this.get('/facts/:id', function({facts}, {params}) {
-    let fact = facts.find(params.id);
-    return this.serialize(fact);
+    return facts.find(params.id);
   });
 }
